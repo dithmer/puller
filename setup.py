25e9e531c51cc9cb2f545468006d24f8767a8a9e
gruesser <gruesser@adesso-mobile.de>
2021-02-26 13:12:55 +0100

    Up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="puller",  # Replace with your own username
-    version="0.2.0.dev1",
+    version="0.2.0.dev2",
     author="Tim Dithmer",
     author_email="dith.tim@gmail.com",
     description="An automatic repo puller",
